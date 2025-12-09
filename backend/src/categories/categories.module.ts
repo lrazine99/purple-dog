215446b235f82857612ff9910ced77be86161734
Ayoub-ELHanda <elhandaayo@gmail.com>
2025-12-09 23:54:13 +0100

    Auto-rePage de détail d'article ,autoremplissage du prix d'enchère ajouté  Sélection multiple de catégories for article  add order section

--- backend/src/categories/categories.module.ts
+++ backend/src/categories/categories.module.ts
@@ -1,14 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
+import { Item } from '../items/entities/item.entity';
+import { ItemCategory } from '../items/entities/item-category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [TypeOrmModule.forFeature([Category, Item, ItemCategory])],
   controllers: [CategoriesController],
   providers: [CategoriesService],
   exports: [CategoriesService],
 })
-export class CategoriesModule {}
+export class CategoriesModule implements OnModuleInit {
+  constructor(private readonly categoriesService: CategoriesService) {}
 
+  async onModuleInit() {
+    // Ensure default category exists on startup
+    await this.categoriesService.ensureDefaultCategory();
+  }
+}
